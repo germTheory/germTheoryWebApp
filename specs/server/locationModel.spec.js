8bc863badaefd5fabe2efe20287b6cdb6c3e361c
Jose Joaquin <jomerinog@gmail.com>
2014-11-05 15:43:25 -0800

    merge upstream

--- specs/server/locationModel.spec.js
+++ specs/server/locationModel.spec.js
@@ -1,10 +1,10 @@
 var should = require('should');
 
 // TODO: implement location and a db config.
-//var Location = require('../../server/locations/locationModel');
+var Location = require('../../server/locations/locationModel');
 describe('Location Model',function(){
   xit('should be awesome',function(){
-    //Location.hasTimestamps.should.be.a('function');
+    Location.hasTimestamps.should.be.a('function');
   })
 
 })
