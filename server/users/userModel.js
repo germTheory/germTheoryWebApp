9fbecc40024bbd5677e5196453bbaa427729abc8
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-11-04 23:08:47 -0800

    Begin building User model. Add pg and bookshelf dependencies.

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -1,5 +1,34 @@
 var bcrypt   = require('bcrypt-nodejs'),
     Q        = require('q'),
+    Bookshelf = require('bookshelf');
     SALT_WORK_FACTOR  = 10;
 
-module.exports = {}; // fill this in with a DB model for our user
+var Users = Bookshelf.Model.extend({
+
+  tableName: 'documents',
+
+  constructor: function() {
+    bookshelf.Model.apply(this, arguments);
+    this.on('saving', function(model, attrs, options) {
+      options.query.where('type', '=', 'book');
+    });
+  }
+
+});
+
+//TODO: Implement addUser, likely using Google OAUTH and jwt
+var addUser = function(data){
+
+}
+// TODO: Implement findUser
+var _findUser = function(params){
+
+}
+
+// TODO: Implement FetchUserLocations
+var _fetchUserLocations = function(){
+
+}
+
+
+module.exports = Bookshelf.model('Users', Users);
