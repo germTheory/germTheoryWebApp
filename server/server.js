cec6362492f42767895730ec2e5ac5245aa38542
suprbh <supb27@yahoo.com>
2014-11-05 13:16:04 -0800

    restructured knex

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
 var express = require('express');
 
-// var dbConfig = require('./config/dbConfig.js');
-// var knex = require('knex')(dbConfig);
-// var bookshelf = require('bookshelf')(knex);
+var dbCreds = require('./config/dbCreds.js');
+var knex = require('knex')(dbCreds);
+var bookshelf = require('bookshelf')(knex);
 
 var app = express();
 
 // to use bookshelf elsewhere in the app, simply write 'var bookshelf = app.get('bookshelf');'
-// app.set('bookshelf', bookshelf);
+app.set('bookshelf', bookshelf);
+app.set('knex', knex);
 
 // configure our server with all the middleware and routing
 require('./config/middleware.js')(app, express);
+
+// configure our server with the knex schema setup
+require('./config/dbConfig.js')(app);
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
