69d540fab80b941e3b65aea4d85aaee08b9a56c9
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-11-04 19:13:41 -0800

    Add location files and update middleware to allow calls to location api endpoints.

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -3,6 +3,8 @@ var userController = require('./userController.js');
 
 module.exports = function (app) {
   // our app is the userRouter injected from middleware.js
+
+  // Basic user routes
   app.post('/signin', userController.signin);
   app.post('/signup', userController.signup);
   app.get('/signedin', userController.checkAuth);
