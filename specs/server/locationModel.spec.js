b7b2a6ccb8672c2f5e2a2eb85bb72992a33ca01e
Jose Joaquin <jomerinog@gmail.com>
2014-11-06 15:18:17 -0800

    create insertion tests and ignore angular in jshintrc

--- specs/server/locationModel.spec.js
+++ specs/server/locationModel.spec.js
@@ -1,10 +1,33 @@
-var should = require('should');
+var db = require('../../server/database/dbSchema.js');
+var Location = db.Location;
 
-// TODO: implement location and a db config.
-var Location = require('../../server/locations/locationModel');
 describe('Location Model',function(){
-  xit('should be awesome',function(){
-    Location.hasTimestamps.should.be.a('function');
-  })
+  var lastid;
+  beforeEach(function(){
+    db.sequelize.sync().success(function(){
+      db.User.create({name: 'jose'}).success(function(user){
+        lastid = user.values.id;
+      });
 
-})
+    });
+  });
+  it('should create rows in database',function(done){
+    db.Location.create({ userId: lastid, latitude: 12.252, longitude: 21.5234234  })
+    .then(function(model){
+      done();
+    },
+    function(error){
+      done(error);
+    });
+  });
+  it('should fail if parameters are wrong',function(done){
+    db.Location.create({ userId: lastid, latitude: 'a'+12.252, longitude: 21.5234234  })
+    .then(function(model){
+      done('Error, expected db operation to fail, but it worked.');
+    },
+    function(error){
+      done();
+    });
+  });
+
+});
