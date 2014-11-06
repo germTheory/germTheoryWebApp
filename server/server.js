8fc4b30a823ff6da1e371a4ba330befdbfd1c6c6
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-11-05 16:30:56 -0800

    Change back to dbCreds.js

--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
 var express = require('express');
 
-var db = require('./config/dbConfig.js');
-// var knex = require('knex')(dbCreds);
-// var bookshelf = require('bookshelf')(knex);
+var dbCreds = require('./config/dbCreds.js');
+var knex = require('knex')(dbCreds);
+var bookshelf = require('bookshelf')(knex);
 
 var app = express();
 
 // to use bookshelf elsewhere in the app, simply write 'var bookshelf = app.get('bookshelf');'
-// app.set('bookshelf', bookshelf);
-// app.set('knex', knex);
+app.set('bookshelf', bookshelf);
+app.set('knex', knex);
 
 // configure our server with all the middleware and routing
 require('./config/middleware.js')(app, express);
 
 // configure our server with the knex schema setup
-// require('./config/dbConfig.js');
+require('./config/dbConfig.js')(app);
 
 // set port
 var port = process.env.PORT || 4568;
