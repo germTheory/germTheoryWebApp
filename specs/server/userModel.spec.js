36fc8972d027cb326b38dca744d95eedb07e40dd
jamesongamble <jamesongamble@gmail.com>
2014-11-05 19:32:54 -0800

    Comments out new tests

--- specs/server/userModel.spec.js
+++ specs/server/userModel.spec.js
@@ -8,29 +8,29 @@ var expect = require('../../node_modules/chai/chai').expect;
 var express = require('express');
 
 
-  xit('Database should have a function called _addUser', function() {
-    User.addUser.should.be.a('function');
-  })
+//   xit('Database should have a function called _addUser', function() {
+//     User.addUser.should.be.a('function');
+//   })
 
-  xit('It should add a user to the database', function(done) {
-    describe('GET /users', function(){
-      it('respond with json', function(done){
-        request(app)
-          .get('/user')
-          .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(200, done);
-      })
-    })
-  })
+//   xit('It should add a user to the database', function(done) {
+//     describe('GET /users', function(){
+//       it('respond with json', function(done){
+//         request(app)
+//           .get('/user')
+//           .set('Accept', 'application/json')
+//           .expect('Content-Type', /json/)
+//           .expect(200, done);
+//       })
+//     })
+//   })
 
-  xit('Database should have a function called diseases', function() {
-    User.findUser.should.be.a('function');
-  })
+//   xit('Database should have a function called diseases', function() {
+//     User.findUser.should.be.a('function');
+//   })
 
-  xit('Database should have a function called location', function() {
-    User.fetchUserLocations.should.be.a('function');
-  })
+//   xit('Database should have a function called location', function() {
+//     User.fetchUserLocations.should.be.a('function');
+//   })
 
 
-})
+// })
