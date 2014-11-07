e1f7ee1e64a12129aef63655e91be7b7e90098ba
suprbh <supb27@yahoo.com>
2014-11-06 20:59:36 -0800

    User controller related changes and Specs for tests

--- specs/server/userModel.spec.js
+++ specs/server/userModel.spec.js
@@ -1,28 +1,65 @@
 // var request = require('supertest');
+var expect = require('expect.js');
 var should = require('should');
-// var User = require('../../users/userModel');
-// var dbCreds = require('./dbCreds_example.js');
-// var knex = require('knex')(dbCreds);
-// var db = require('bookshelf')(knex);
+var sequelize = require('sequelize');
+var userController = require('../../server/controllers/userController.js');
+var db = require('../../server/database/dbSchema.js');
+var request = require('supertest');
 // var expect = require('../../node_modules/chai/chai').expect;
 // var express = require('express');
 
-xdescribe('Location Model',function(){
-  xit('Database should have a function called _addUser', function() {
-    User.addUser.should.be.a('function');
+// Controller tests
+describe('User Controller',function(){
+  it('User Controller should be an object', function() {
+    expect(userController).to.be.an('object');
   })
 
-  xit('It should add a user to the database', function(done) {
-    describe('GET /users', function(){
-      it('respond with json', function(done){
-        request(app)
-          .get('/user')
-          .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(200, done);
+  it('should have a method called saveUser', function() {
+    expect(userController.saveUser).to.be.ok;
+  })
+
+  it('should have a method called findUser', function() {
+    expect(userController.findUser).to.be.ok;
+  })
+
+   it('should have a method called signin', function() {
+    expect(userController.signin).to.be.ok;
+  })
+
+  it('should have a method called signup', function() {
+    expect(userController.signup).to.be.ok;
+  })
+
+});
+
+describe('User',function() {
+
+  it('It should add a user to the database', function(done) {
+      userController.saveUser("test", function(user){
+        console.log("saveUser in test: ", user);
+        expect(user).to.be('test');
+        done();
+      })
+
+    })
+
+    it('It should find User from the database', function(done){
+      userController.findUser("test", function(user){
+        console.log("findUser in test: ", user);
+        expect(user).to.be('test');
+        done();
       })
     })
-  })
+
+    // describe('GET /users', function() {
+
+    xit('respond with json', function(done) {
+      request(app)
+        .get('/user')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    })
 
   xit('Database should have a function called diseases', function() {
     User.findUser.should.be.a('function');
@@ -31,6 +68,5 @@ xdescribe('Location Model',function(){
   xit('Database should have a function called location', function() {
     User.fetchUserLocations.should.be.a('function');
   })
-
 
 })
