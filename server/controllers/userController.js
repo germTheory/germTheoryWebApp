e94bbe7c6c4a708e33637e6104fff152dedce268
suprbh <supb27@yahoo.com>
2014-11-07 14:52:13 -0800

    minor changes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,10 +10,8 @@ var userController = {
   Signin to the app if already a user.
   */
   signin: function (req, res, next) {
-    console.log("Here 1");
     // TODO: remove hack
-    // var username = req.body.username;
-    var username = "test";
+    var username = req.body.username;
 
     db.findUser(_username, function(results){
       if (results.length === 0) {
@@ -32,9 +30,7 @@ var userController = {
   */
   signup: function (req, res, next) {
     // TODO: remove hack
-    // var username  = req.body.username,
-    var username = "hello";
-    console.log("In signup");
+    var username  = req.body.username,
 
     // TO BE IMPLEMENTED WITH OAUTH
 
