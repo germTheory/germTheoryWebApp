d911ce96cfb991aa5c2e3a0d8d2a12ab21c07910
jamesongamble <jamesongamble@gmail.com>
2014-11-07 11:07:41 -0800

    Creates a dashboard with buttons/options in mobile app

--- mobile/www/js/controllers.js
+++ mobile/www/js/controllers.js
@@ -1,6 +1,11 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope, DashboardOptions) {
+  $scope.options = DashboardOptions.all();
+})
+
+.controller('DashDetailCtrl', function($scope, $stateParams, DashboardOptions) {
+  $scope.option = DashboardOptions.get($stateParams.optionId);
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
