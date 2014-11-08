3e9dc372be74f52600a72325ec452383df93f558
jamesongamble <jamesongamble@gmail.com>
2014-11-08 14:03:38 -0800

    Sets splash page to login.html

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -106,7 +106,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/settings');
+  $urlRouterProvider.otherwise('/tab/login');
 
 });
 
