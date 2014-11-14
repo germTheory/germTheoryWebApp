5138adf0d44169849ada409f89f8b4398512bc76
Jose Joaquin <jomerinog@gmail.com>
2014-11-13 17:22:17 -0800

    adding seed for locations

--- server/config/seed.js
+++ server/config/seed.js
@@ -1,13 +1,30 @@
+var db = require('../../server/database/dbSchema.js');
+
+// generates an exponential distribution random variable to model time step randomness
 var exp = function(lambda){
   var res = Math.log(1-Math.random())/(1/-lambda);
-  console.log(res);
   return res;
 };
-
-var simulate = function(start,end,lambda){
+/**
+ * Creates n points and simulates their movements between two times and stores the values in the database.
+ *
+ * users: an array of user ids. (only numbers)
+ * start: start unix timestamp
+ * end: end unix timestamp
+ * lambda: exponential distribution lambda
+ *
+ * returns an array with the values that we tried to insert.
+ *
+ * note: The returned array is the result of the simulation that we attempt to insert into the database,
+ * not the inserted values themselves.
+ */
+var generateLocations = function(users,start,end,lambda){
   var t = start;
-
-  var people = [];
+  var n = Array.isArray(users)?users.length:users;
+  var results = [];
+  var saveIntoArray = function(item){
+    results.push(item);
+  };
 
   console.log('Started Simulation start: ',start,' end: ',end,lambda, '. \n Lasts for ', end-start,'ms');
   var events = [];
@@ -16,9 +33,12 @@ var simulate = function(start,end,lambda){
   var insertEventSorted = function(event){
     // put it in the end of the list
     events.push(event);
+    if(events.length === 0 ){
+      return;
+    }
     // sort it into place with insertion sort.
-    for( i = events.length-1; i >=1; i-- ){
-      if( events[i] > events[i-1] ){
+    for(var i = events.length-1; i >=1; i-- ){
+      if( events[i].time > events[i-1].time ){
         var aux = events[i];
         events[i] = events[i-1];
         events[i-1] = aux;
@@ -28,51 +48,96 @@ var simulate = function(start,end,lambda){
     }
   };
 
+  // Different functions for pdating position
+  var updaters = {
+    leftCircle: function(){
+      var interpolation = this.interpolator;
+      var p = Math.abs(this.worklng-this.homelng)+Math.abs(this.worklat-this.homelat);
+      var lat = this.homelat + (this.worklat - this.homelat)/2 + 0.5*p*Math.cos(interpolation);
+      var lng = this.homelng + (this.worklng - this.homelng)/2 + 0.5*p*Math.sin(interpolation);
+      return [lat,lng];
+    },
+    rightCircle: function(){
+      var interpolation = this.interpolator;
+      var p = Math.abs(this.worklng-this.homelng)+Math.abs(this.worklat-this.homelat);
+      var lat = this.homelat + (this.worklat - this.homelat)/2 + 0.5*p*Math.cos(interpolation);
+      var lng = this.homelng + (this.worklng - this.homelng)/2 + 0.5*p*Math.sin(-interpolation);
+      return [lat,lng];
+    },
+    diagonal: function() {
+      var interpolation = this.interpolator;
+      var p = Math.abs(this.worklng-this.homelng)+Math.abs(this.worklat-this.homelat);
+      var lat = this.homelat + (this.worklat - this.homelat)/2 + 0.5*p*Math.cos(interpolation);
+      var lng = this.homelng + (this.worklng - this.homelng)/2 + 0.5*p*Math.cos(interpolation);
+      return [lat,lng];
+    },
+    lowDiagonal: function(){
+      var interpolation = this.interpolator;
+      var p = Math.abs(this.worklng-this.homelng)+Math.abs(this.worklat-this.homelat);
+      var lat = this.homelat + (this.worklat - this.homelat)/2 + 0.5*p*Math.sin(interpolation);
+      var lng = this.homelng + (this.worklng - this.homelng)/2 + 0.5*p*Math.sin(-interpolation);
+      return [lat,lng];
+    }
+  };
   // generate initial set of events, 1 per person
-  for(var i = 0; i < 10; i++){
-    people.push();
+  for(var i = 0; i < n; i++){
     var event = {
-      id: i,
+      id: Array.isArray(users)? users[i]:i+1,
       homelat: 37.789599 + (37.757034 - 37.789599) * Math.random(),
       homelng: -122.452755 + (-122.390785 + 122.452755) * Math.random(),
       worklat: 37.787022 + (37.757034 - 37.787022) * Math.random(),
       worklng: -122.432156 + (-122.390785 + 122.432156) * Math.random(),
       interpolator: 2*Math.PI*Math.random(), // offset everyone's cycle
+      update:updaters[(['leftCircle','rightCircle','diagonal','lowDiagonal'])[Math.floor(Math.random()*4)]],
       time: start + exp(lambda)
     };
-    people.push(person);
 
     // now events is sorted, with next event to be popped being the lowest time
     insertEventSorted(event);
 
   }
 
+  // until simulation time is over, create data out of each event and generate the next one.
   while (t < end && events.length ){
 
     // pop the next event.
     var currentEvent = events.pop();
 
     // time since last measurement
-    var step = t; //ms
+    var step = currentEvent.time-t; //ms
+
 
     // update the time
     t = currentEvent.time;
 
-    //insert someone at the position of the event
-
-    var p = Math.abs(currentEvent.worklng-currentEvent.homelng)+Math.abs(currentEvent.worklat-currentEvent.homelat);
-    var lat = currentEvent.homelat + (currentEvent.worklat - currentEvent.homelat)/2 + p*Math.cos(currentEvent.interpolator);
-    var lng = currentEvent.homelng + (currentEvent.worklng - currentEvent.homelng)/2 + p*Math.sin(currentEvent.interpolator);
+    // controls how much to move
+    var interpolation = currentEvent.interpolator;
 
 
-    // update event and insert it on list.
-    currentEvent.interpolator+= 2*Math.PI*step/3600000;
+
+    // gets new positions based on the event update calculation
+    var vals = currentEvent.update();
+
+    saveIntoArray({
+      user_id:currentEvent.id,
+      latitude:vals[0],
+      longitude:vals[1],
+      date:t
+    });
+
+    // modify event interpolator and reinsert it into the future event list
+    currentEvent.interpolator = (interpolation + 2*Math.PI/(100));
 
     //generate next event for this person
     currentEvent.time = t + exp(lambda);
+
+    //make sure to insert events sorted
     insertEventSorted(currentEvent);
 
-    console.log('Executed ',t,lat,lng, 'Time left : ', end-t);
   }
+
+
+  // return the results in case they are needed
+  return results;
 };
-simulate(Date.now()-24*3600*1000, Date.now(),120000);
+exports.simulate = generateLocations;
