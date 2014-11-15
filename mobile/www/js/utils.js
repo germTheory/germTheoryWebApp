3fdab78a187f06b9d4c83ae66484ed1a070ce3b5
jamesongamble <jamesongamble@gmail.com>
2014-11-14 19:33:22 -0800

    Edits some more css for myrisk and report views

--- mobile/www/js/utils.js
+++ mobile/www/js/utils.js
@@ -13,29 +13,4 @@ angular.module('app.utils', ['app.services'])
         console.log(LocalStorage.get('tracking'));
       }
     };
-
-
-    // $scope.pushTrack = function () {
-    //   console.log('got into new pushTrack');
-    // }
-
-    // $scope.pushTrack();
-    // $localstorage.set('tracking', 'false');  
-    // var name = window.localStorage['name'] || 'you';
-    // alert('Hello, ' + name);
-    // console.log($localstorage.get('tracking'));
-
 }]);
-
-
-
-    // $localstorage.toggleTrack = $localstorage.get('tracking');
-    // $localstorage.togglePush = $localstorage.get('pushing');
-    // $localstorage.toggleFb = $localstorage.get('fb');
-
-
-
-    // // $localstorage.set('tracking', 'false');  
-
-    // console.log($localstorage.get('tracking'));
-    // console.log($localstorage.get('pushing'));
