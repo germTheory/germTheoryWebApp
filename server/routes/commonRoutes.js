bc188ae632c6e5922d86d269d049faf0faab6ecb
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-11-17 21:46:58 -0800

    Major changes to server, including new redirects to an admin dashboard.

--- server/routes/commonRoutes.js
+++ server/routes/commonRoutes.js
@@ -4,12 +4,15 @@ var authController = require('../controllers/authController.js');
 var locationController = require('../controllers/locationController.js');
 var caseController = require('../controllers/caseController.js');
 var processingController = require('../controllers/processingController.js');
+var reportController = require('../controllers/reportController.js')
 var passport = require('passport');
 
 module.exports = function (app) {
   // Mobile auth routes
   app.get('/mobile', helpers.isLoggedIn, authController.showMobile);
-
+  app.get('/', helpers.isLoggedIn, function(req, res){
+    res.render('home');
+  })
   // Web auth routes
   app.get('/users', helpers.isLoggedIn, userController.showAllUsers);
   app.get('/users/:id', helpers.isLoggedIn, userController.showUserInfo);
@@ -18,10 +21,7 @@ module.exports = function (app) {
   app.get('/cases', helpers.isLoggedIn, caseController.showAllReportedCase);
   
   // REPORTS
-  app.get('/reports', helpers.isLoggedIn, function(req, res) {
-    res.render('reports');
-  });
-  
+  app.get('/reports', helpers.isLoggedIn, reportController.showAllProximityReports);
   app.post('/reports', helpers.isLoggedIn, processingController.createReport);
   
   app.get('/newReportedCase', helpers.isLoggedIn, function(req, res) {
