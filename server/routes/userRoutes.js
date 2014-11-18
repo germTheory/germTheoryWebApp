e05aef14d96b84f74bea04035ad2d606ab79a98a
jamesongamble <jamesongamble@gmail.com>
2014-11-17 19:45:45 -0800

    Begins working on updating settings page

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,6 +5,6 @@ module.exports = function (app) {
   app.get('/', userController.getAllUsers);
   app.get('/:id', userController.getUser);
   app.post('/', helpers.invalidMethodHandler);
-  app.put('/:id', helpers.invalidMethodHandler);
+  app.put('/:id', userController.getUser);
   app.delete('/:id', helpers.invalidMethodHandler);
 };
