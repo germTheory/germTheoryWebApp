15802ba524f326e998bb5279034f46b2356cf436
Kevin Meurer <kevin.a.meurer@gmail.com>
2014-11-18 16:42:02 -0800

    Add new page to post case outbreak loactions. Updated css for table rows.

--- server/controllers/caseController.js
+++ server/controllers/caseController.js
@@ -1,5 +1,6 @@
 var ReportedCase = require('../database/dbSchema').ReportedCase;
 var Disease = require('../database/dbSchema').Disease;
+var diseaseController = require('./diseaseController.js');
 
 module.exports = {
 
@@ -17,11 +18,14 @@ module.exports = {
   },
 
   createNewCase: function(req, res, next) {
+    var splitDate = req.body.date.split('/');
+    var newDate = new Date(splitDate[2], splitDate[0] - 1, splitDate[1]);
+
     ReportedCase.create({
-      disease_id: req.body.disease_id,
-      latitude: req.body.latitude,
-      longitude: req.body.longitude,
-      date: req.body.date,
+      disease_id: parseInt(req.body.disease_id),
+      latitude: parseFloat(req.body.latitude),
+      longitude: parseFloat(req.body.longitude),
+      date: newDate,
       description: req.body.description })
       .success(function(reportedCase) {
         res.setHeader('Content-Type', 'application/json');
@@ -30,6 +34,7 @@ module.exports = {
       .error(function(err) {
         res.status(400).send({ error: err.name, message: err.message });
       });
+    res.redirect('/cases');
   },
 
   showAllReportedCase: function(req, res, next) {
@@ -37,5 +42,11 @@ module.exports = {
       success(function(reportedCases) {
         res.render('reportedCases', { cases: reportedCases });
       });
+  },
+
+  newReportedCase: function(req, res, next){
+    diseaseController.getAllDiseases(function(diseases){
+      res.render('newReportedCase', { diseases: diseases } )
+    });
   }
 };
--- server/controllers/reportController.js
+++ server/controllers/reportController.js
@@ -1,4 +1,5 @@
 var db = require("../database/dbSchema.js");
+var diseaseController = require("./diseaseController.js")
 
 var _showAllProximityReports = function(req, res, next){
 	db.ProximityReport.findAll({ order: 'id DESC' })
@@ -6,8 +7,15 @@ var _showAllProximityReports = function(req, res, next){
         res.set('Content-Type', 'text/html');
         res.render('reports', { results: results }); 
       });
-}
+};
+
+var _newRiskReport = function(req, res, next){
+	diseaseController.getAllDiseases(function(diseases){
+		res.render('newRiskReport', {diseases: diseases});
+	});
+};
 
 module.exports = {
-	showAllProximityReports: _showAllProximityReports
+	showAllProximityReports: _showAllProximityReports,
+	newRiskReport: _newRiskReport
 }
