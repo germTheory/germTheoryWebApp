7965cd2423b105e0dea3b6e4fc261569a1176c02
Jose Joaquin <jomerinog@gmail.com>
2014-11-21 11:47:55 -0800

    added failing tests for routes

---
+++ specs/server/auth.spec.js
@@ -0,0 +1,14 @@
+var db = require('../../server/database/dbSchema.js');
+var request = require('supertest');
+var expect = require('chai').expect;
+var app = require('../../server/server.js');
+
+describe('Auth specs', function() {
+
+  it('should return a 401 if not logged in',function(done){
+    request(app)
+      .get('/api/secure')
+      .expect(401, done);
+  })
+
+});
